05d9ab1d5b5cd056f4b4b6de450ab46ec41338a9
Marcus Hermansson <marcus@hmwh.se>
2026-09-22 23:20:21 +0200

    Wire quasi-stationary theorem layer

--- PermanssonLean.lean
+++ PermanssonLean.lean
@@ -14,6 +14,7 @@ import PermanssonLean.Regime.Persistence
 import PermanssonLean.Regime.Occupation
 import PermanssonLean.Regime.GeneratedRegime
 import PermanssonLean.Regime.PolishDescriptor
+import PermanssonLean.Regime.QuasiStationary
 import PermanssonLean.Regime.Property
 import PermanssonLean.Regime.Constitution
 import PermanssonLean.Regime.Permansson
